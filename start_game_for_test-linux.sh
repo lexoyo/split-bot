2fa10b0fecd34f22c80bd727a899b3c195a30450
lexoyo <a.hoyau@silexlabs.org>
2015-04-12 14:51:29 -0400

    readme and linux/mac start game scripts

---
+++ start_game_for_test-linux.sh
@@ -0,0 +1,2 @@
+# linux
+/usr/games/pyrogenesis -nosound -quickstart -autostart=cantabrian_highlands -autostart-random -mod "Cantabrian Highlands" -autostart-ai=1:Splitbot -autostart-ai=2:aegis -autostart-players=2
---
+++ start_game_for_test-mac.sh
@@ -0,0 +1,2 @@
+# mac
+/Applications/0ad.app/Contents/MacOS/pyrogenesis -nosound -quickstart -autostart=cantabrian_highlands -autostart-random -mod "Cantabrian Highlands" -autostart-ai=1:SplitBot -autostart-ai=2:aegis -autostart-players=2
